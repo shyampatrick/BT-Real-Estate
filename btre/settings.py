ae12f1e3129838666c8df95fd675f6c976c42add
Shyam <patrick.shyamkumar@gmail.com>
2019-12-03 14:44:24 +0800

    Add Fixtures DB Seeding + Bug Fixes

--- btre/settings.py
+++ btre/settings.py
@@ -140,6 +140,12 @@ STATICFILES_DIRS = [
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
+
+FIXTURE_DIRS = [
+    os.path.join(BASE_DIR, 'fixtures')
+]
+
+
 from django.contrib.messages import constants as messages
 MESSAGE_TAGS = {
     messages.ERROR: 'danger',
--- contacts/views.py
+++ contacts/views.py
@@ -21,18 +21,18 @@ def contact(request):
             has_contacted = Contact.objects.all().filter(listing_id=listing_id, user_id=user_id)
             if has_contacted:
                 messages.error(request, 'You have an existing inquiry. Only one is allowed per listing')
-                return redirect('listings/' + listing_id)
+                return redirect('/listings/' + listing_id)
 
-        contact = Contact(listing=listing, listing_id=listing_id, name=name, email=email, phone=phone, message=message, user_id=user_id, realtor_email=realtor_email)
+        contact = Contact(listing=listing, listing_id=listing_id, name=name, email=email, phone=phone, message=message, user_id=user_id)
         contact.save()
 
-        send_mail(
-            'Property Listing Inquiry',
-            'There has been an inquiry for ' + listing + '. Sign into admin panel for more info'
-            'sykron@gmail.com',
-            [realtor_email],
-            fail_silently=False
-        )
+        # send_mail(
+        #     'Property Listing Inquiry',
+        #     'There has been an inquiry for ' + listing + '. Sign into admin panel for more info'
+        #     'sykron@gmail.com',
+        #     [realtor_email],
+        #     fail_silently=False
+        # )
 
         messages.success(request, 'Your request has been submitted, a realtor will get back to you shortly...')
-        return redirect('listings/' + listing_id)
+        return redirect('/listings/' + listing_id)
